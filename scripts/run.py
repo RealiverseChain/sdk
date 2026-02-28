788f967b7862d2e1e1f6b42e0db4837778e354d6
Realiverse <pekekgaming2@gmail.com>
2026-02-28 09:44:46 +0700

    Create run.py

---
+++ scripts/run.py
@@ -0,0 +1,15 @@
+import uvicorn
+
+def start():
+
+    uvicorn.run(
+        "api.server:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
+
+
+if __name__ == "__main__":
+
+    start()
